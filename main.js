b60c838830d9abcc1d5d3e89859adc95598709f1
Douyemiene <viktormiene.vm@gmail.com>
2021-09-19 20:54:54 +0100

    Feat: complete rewrite of functionality and styles

--- main.js
+++ main.js
@@ -3,21 +3,63 @@ const taskContainer = document.querySelector("#task-container");
 const addTodo = document.querySelector("#addButton");
 const todoInput = document.querySelector("#todoInput");
 
-const completeTodo = (item) => {
-  item.setAttribute("data-todo-item", "completed");
+const createEditAndDelete = (input, todoItem) => {
+  const edit = document.createElement("button");
+  edit.innerHTML = `<i class="far fa-edit fa-lg"></i>`;
+
+  const del = document.createElement("button");
+  del.innerHTML = `<i class="far fa-trash-alt fa-lg"></i>`;
+
+  const ok = document.createElement("button");
+  ok.innerHTML = `<i class="fas fa-check fa-lg"></i>`;
+
+  ok.style.display = "none";
+
+  edit.addEventListener("click", () => {
+    input.disabled = false;
+    input.classList.add("focused");
+    ok.style.display = "block";
+  });
+
+  del.addEventListener("click", () => {
+    taskContainer.removeChild(todoItem);
+  });
+
+  ok.addEventListener("click", function () {
+    input.disabled = "true";
+    this.style.display = "none";
+    input.classList.remove("focused");
+  });
+
+  return [edit, del, ok];
 };
 
-const addListItem = () => {
-  const inputValue = todoInput.value;
-  //check if the a value was inputted
-  if (inputValue) {
-    const listIem = document.createElement("li");
-    listIem.setAttribute("data-todo-item", "");
-    listIem.innerText = todoInput.value;
-    taskContainer.appendChild(listIem);
+const createListItem = () => {
+  const todoItem = document.createElement("div");
+  const todoItemInput = document.createElement("input");
+  const [edit, del, ok] = createEditAndDelete(todoItemInput, todoItem);
+  // todoItem.classList("todo-item");
+  todoItem.classList.add("todo-item");
+  todoItemInput.classList.add("todo-item-input");
 
-    listIem.addEventListener("click", () => completeTodo(listIem));
+  //give disabled input a value
+  todoItemInput.value = todoInput.value;
+  todoItemInput.disabled = true;
+
+  todoItem.appendChild(todoItemInput);
+  todoItem.appendChild(edit);
+  todoItem.appendChild(ok);
+  todoItem.appendChild(del);
+
+  return todoItem;
+};
+
+const AppendListItem = () => {
+  //check if the a value was inputted
+  if (todoInput.value) {
+    const todoItem = createListItem();
+    taskContainer.appendChild(todoItem);
   }
 };
 
-addTodo.addEventListener("click", addListItem);
+addTodo.addEventListener("click", AppendListItem);
